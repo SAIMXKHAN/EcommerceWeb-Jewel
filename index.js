2507cbd287e76d6cb78ec48a24df883501250b84
SAIMXKHAN <saim.tauseef27@gmail.com>
2025-01-13 16:56:26 -0800

    resposiveness completed

--- index.js
+++ index.js
@@ -1,151 +1,6 @@
 // Function to render products
    // Product data  
-   const ringsProduct = [
-    {
-      image: "jewel-Images/goldring.webp",
-      name: "Dune Quadra Stack - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.1,799.00 PKR",
-      tag: "Sale",
-    },
-    {
-      name: "Iced Out Luxe Zircon Ring - 18K",
-      originalPrice: "Rs.1,999.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Irregular Baroque Pearl Ring - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Link Up Ring - 18K",
-      originalPrice: "Rs.2,199.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Lea Baguette X Curb Ring",
-      originalPrice: "Rs.2,699.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      image: "jewel-Images/goldring.webp",
-      name: "Dune Quadra Stack - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.1,799.00 PKR",
-      tag: "Sale",
-    },
-    {
-      name: "Iced Out Luxe Zircon Ring - 18K",
-      originalPrice: "Rs.1,999.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Irregular Baroque Pearl Ring - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Link Up Ring - 18K",
-      originalPrice: "Rs.2,199.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Lea Baguette X Curb Ring",
-      originalPrice: "Rs.2,699.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-  ];
-
-  const braceletsProduct = [
-    {
-      image: "jewel-Images/goldring.webp",
-      name: "Dune Quadra Stack - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.1,799.00 PKR",
-      tag: "Sale",
-    },
-    {
-      name: "Iced Out Luxe Zircon Ring - 18K",
-      originalPrice: "Rs.1,999.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Irregular Baroque Pearl Ring - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Link Up Ring - 18K",
-      originalPrice: "Rs.2,199.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Lea Baguette X Curb Ring",
-      originalPrice: "Rs.2,699.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      image: "jewel-Images/goldring.webp",
-      name: "Dune Quadra Stack - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.1,799.00 PKR",
-      tag: "Sale",
-    },
-    {
-      name: "Iced Out Luxe Zircon Ring - 18K",
-      originalPrice: "Rs.1,999.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Irregular Baroque Pearl Ring - 18K",
-      originalPrice: "Rs.2,599.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Link Up Ring - 18K",
-      originalPrice: "Rs.2,199.00 PKR",
-      salePrice: "Rs.1,699.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-    {
-      name: "Lea Baguette X Curb Ring",
-      originalPrice: "Rs.2,699.00 PKR",
-      salePrice: "Rs.2,199.00 PKR",
-      tag: "Sale",
-      image: "jewel-Images/goldring.webp",
-    },
-  ]
-
+ 
    const products = [
     {
         id: 1,
@@ -251,6 +106,153 @@ function renderProducts() {
   container.innerHTML = productsHTML;
 }
 
+
+const ringsProduct = [
+  {
+    image: "jewel-Images/goldring.webp",
+    name: "Dune Quadra Stack - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.1,799.00 PKR",
+    tag: "Sale",
+  },
+  {
+    name: "Iced Out Luxe Zircon Ring - 18K",
+    originalPrice: "Rs.1,999.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Irregular Baroque Pearl Ring - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Link Up Ring - 18K",
+    originalPrice: "Rs.2,199.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Lea Baguette X Curb Ring",
+    originalPrice: "Rs.2,699.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    image: "jewel-Images/goldring.webp",
+    name: "Dune Quadra Stack - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.1,799.00 PKR",
+    tag: "Sale",
+  },
+  {
+    name: "Iced Out Luxe Zircon Ring - 18K",
+    originalPrice: "Rs.1,999.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Irregular Baroque Pearl Ring - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Link Up Ring - 18K",
+    originalPrice: "Rs.2,199.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Lea Baguette X Curb Ring",
+    originalPrice: "Rs.2,699.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+];
+
+const braceletsProduct = [
+  {
+    image: "jewel-Images/goldring.webp",
+    name: "Dune Quadra Stack - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.1,799.00 PKR",
+    tag: "Sale",
+  },
+  {
+    name: "Iced Out Luxe Zircon Ring - 18K",
+    originalPrice: "Rs.1,999.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Irregular Baroque Pearl Ring - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Link Up Ring - 18K",
+    originalPrice: "Rs.2,199.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Lea Baguette X Curb Ring",
+    originalPrice: "Rs.2,699.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    image: "jewel-Images/goldring.webp",
+    name: "Dune Quadra Stack - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.1,799.00 PKR",
+    tag: "Sale",
+  },
+  {
+    name: "Iced Out Luxe Zircon Ring - 18K",
+    originalPrice: "Rs.1,999.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Irregular Baroque Pearl Ring - 18K",
+    originalPrice: "Rs.2,599.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Link Up Ring - 18K",
+    originalPrice: "Rs.2,199.00 PKR",
+    salePrice: "Rs.1,699.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+  {
+    name: "Lea Baguette X Curb Ring",
+    originalPrice: "Rs.2,699.00 PKR",
+    salePrice: "Rs.2,199.00 PKR",
+    tag: "Sale",
+    image: "jewel-Images/goldring.webp",
+  },
+]
+
 function renderRingsProducts() {
   const container = document.getElementById("rings-productsContainer");
 
@@ -293,14 +295,14 @@ function renderBraceletsProducts() {
   const container = document.getElementById("bracelets-productsContainer");
 
   const productsHTML = braceletsProduct.map((product) => `
-            <div class="col-12 col-sm-6 col-lg-3">
+            <div>
                 <div class="card product-card border-0">
                     
                     <img src="${product.image}" 
                          class="card-img-top product-image" 
                          alt="${product.name}">
                          
-                    <div class="card-body text-center">
+                    <div class="card text-center">
                     ${
                         product.onSale
                           ? '<span class="sale-badge">Sale</span>'
@@ -347,7 +349,7 @@ function initializeSlider() {
         {
           breakpoint: 576, // For mobile
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 2,
             slidesToScroll: 1,
           },
         },
@@ -362,7 +364,8 @@ function initializeSlider() {
       arrows: true, // Enable navigation arrows
       dots: false, // Disable dots
       infinite: false, // Disable infinite scrolling
-      prevArrow: '<p type="button" class="slick-prev"></button>',
+
+      prevArrow: '<button type="button" class="slick-prev"></button>',
       nextArrow: '<button type="button" class="slick-next"></button>',
       responsive: [
         {
@@ -384,8 +387,6 @@ function initializeSlider() {
   }
 
   // Ensure the slider is initialized after the DOM is ready
-
-
   document.addEventListener("DOMContentLoaded", () => {
     renderRingsProducts(); // Render Rings products
     renderBraceletsProducts(); // Render Bracelets products
@@ -398,4 +399,3 @@ function initializeSlider() {
 // Render products when page loads
 document.addEventListener("DOMContentLoaded", renderProducts);
 
-let slickslides = document.getElementById(slickSlides);
